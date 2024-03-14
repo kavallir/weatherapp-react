5e7cb02cf8b65e2c4ab7e453cb09a10c1e71adf2
kavallir <152403875+kavallir@users.noreply.github.com>
2024-03-14 17:54:54 +0100

    Added API part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  let [city, setCity] = useState();
+  let [weather, setWeather] = useState({});
+  let [outputText, setOutputText] = useState(false);
+
+  function showTemperature(response) {
+    setOutputText(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "0a521eaf234a3a56f45252fac3c737ad";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+
+  if (outputText) {
+    return (
+      <div className="App">
+        <h1>Weather app</h1>
+        <div className="Search">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Type a city..."
+              onChange={updateCity}
+            />
+            <input type="submit" value="Search" />
+          </form>
+          <ul>
+            <div>The temperature is {Math.round(weather.temperature)}°C</div>
+            <div>Description: {weather.description}</div>
+            <div>The humidity is: {weather.humidity}%</div>
+            <div>
+              <img src={weather.icon} alt="Weather Icon" />
+            </div>
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>Weather app</h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Type a city..."
+              onChange={updateCity}
+            />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
